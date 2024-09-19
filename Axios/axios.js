37f3faa015f145c91f95ff494f2549c13799ecce
first <garvitjainraju@gmail.com>
2024-09-19 16:45:01 +0530

    Completed

--- Axios/axios.js
+++ Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:`http://localhost:5001/api/v1/user`,
+    baseURL:`https://billbackend-e41n.onrender.com/api/v1/user`,
     withCredentials:true
 })
 export default instance
