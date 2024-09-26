084c6b05402a4bd1de1fb05396ba93fbeac83d02
first <garvitjainraju@gmail.com>
2024-09-26 12:56:06 +0530

    new

--- Axios/axios.js
+++ Axios/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL:`https://billbackend-e41n.onrender.com/api/v1/user`,
-    baseURL:"http://localhost:5001/api/v1/user",
+    baseURL:`https://billbackend-e41n.onrender.com/api/v1/user`,
+    // baseURL:"http://localhost:5001/api/v1/user",
 
     withCredentials:true
 })
